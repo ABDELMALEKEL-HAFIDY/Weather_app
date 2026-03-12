4c40398f4bfa088906fd0ac7dc17deb46f11f749
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-12 20:48:56 +0000

    refactor base url

--- lib/core/services/weather_service.dart
+++ lib/core/services/weather_service.dart
@@ -3,11 +3,18 @@ import 'package:weather_app/core/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
-
+  final String baseUrl = 'https://api.weatherapi.com/v1';
   WeatherService(this.dio);
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
     Response response = await dio.get(
-      'https://api.weatherapi.com/v1/forecast.json?key=bb2280fbda7d45b0ad904204261203&q=$cityName&days=1',
+      '$baseUrl/forecast.json?key=bb2280fbda7d45b0ad904204261203&q=$cityName&days=1',
+    );
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
+  Future<WeatherModel> getForcast({required String cityName}) async {
+    Response response = await dio.get(
+      '$baseUrl/forecast.json?key=bb2280fbda7d45b0ad904204261203&q=$cityName&days=1',
     );
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
     return weatherModel;
