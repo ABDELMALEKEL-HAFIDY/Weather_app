5bfb069a4f9182693f4907e82206e013a3d7049a
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-12 05:15:43 +0000

    create weater service and add package dio

--- lib/core/services/weather_service.dart
+++ lib/core/services/weather_service.dart
@@ -1,1 +1,13 @@
-class WeatherService {}
+import 'package:dio/dio.dart';
+import 'package:weather_app/core/models/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+
+  WeatherService(this.dio);
+  WeatherModel getCurrentWeather() {
+    dio.get(
+      'https://api.weatherapi.com/v1/forecast.json?key=bb2280fbda7d45b0ad904204261203&q=London&days=1',
+    );
+  }
+}
