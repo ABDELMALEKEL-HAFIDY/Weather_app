80888105f01b90abb53f8ca45f7d19f837d44ef0
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-12 01:53:46 +0000

    create weather models part one

--- lib/core/models/weather_model.dart
+++ lib/core/models/weather_model.dart
@@ -1,16 +1,25 @@
 class WeatherModel {
   final String cityName;
-  final String conditionText;
-  final String iconUrl;
-
-  final double tempC;
-  final double tempF;
-
-  final double maxTempC;
-  final double minTempC;
-  final double avgTempC;
-
+  final String weatherCondition;
+  final String? icon;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
+  final double avgTemp;
   final double maxWindKph;
   final int humidity;
   final int chanceOfRain;
+
+  WeatherModel({
+    required this.cityName,
+    required this.weatherCondition,
+    this.icon,
+    required this.temp,
+    required this.maxTemp,
+    required this.minTemp,
+    required this.avgTemp,
+    required this.maxWindKph,
+    required this.humidity,
+    required this.chanceOfRain,
+  });
 }
