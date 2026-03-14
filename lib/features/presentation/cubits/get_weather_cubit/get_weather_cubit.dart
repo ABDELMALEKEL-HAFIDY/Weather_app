40f29a9f582cd7e2331237262f46b2360d64fb1d
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-14 02:01:11 +0000

    create function for logic cubit

--- lib/features/presentation/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/features/presentation/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,6 +1,16 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/core/models/weather_model.dart';
+import 'package:weather_app/core/services/weather_service.dart';
 import 'package:weather_app/features/presentation/cubits/get_weather_cubit/get_weather_state.dart';
+import 'package:weather_app/features/presentation/views/search_view.dart';
 
-class GetWeatherCubit extends Cubit <WeatherState>{
+class GetWeatherCubit extends Cubit<WeatherState> {
   GetWeatherCubit(super.initialState);
+
+  getWeather({required String cityName}) async {
+    WeatherModel weatherModel = await WeatherService(
+      Dio(),
+    ).getCurrentWeather(cityName: cityName);
+  }
 }
