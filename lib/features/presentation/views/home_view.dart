ba87ad8f97b4546f1acc66b5af5937c40d73ce76
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-14 03:31:56 +0000

    integrate cubit with condition

--- lib/features/presentation/views/home_view.dart
+++ lib/features/presentation/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:weather_app/features/presentation/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/features/presentation/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/features/presentation/views/search_view.dart';
 import 'package:weather_app/features/presentation/views/weather_info_body.dart';
 import 'package:weather_app/features/widgets/no_weather_body.dart';
@@ -17,14 +20,14 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfff2f2f2),
-      appBar:  AppBar(
+      appBar: AppBar(
         backgroundColor: const Color(0xfff2f2f2),
         leading: IconButton(
           icon: Icon(Iconsax.menu_1_copy, color: Color(0xff94D2BD)),
           onPressed: () {},
         ),
         actions: [
-           Padding(
+          Padding(
             padding: const EdgeInsets.only(right: 15),
             child: IconButton(
               icon: Icon(Iconsax.search_normal_copy, color: Color(0xff94D2BD)),
@@ -35,8 +38,18 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
       ),
-      body: NoWeatherBody(),
-      
+      body: BlocBuilder<GetWeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherInitialState) {
+            return NoWeatherBody();
+          } else if (state is WeatherLoadedSate) {
+            return WeatherInfoBody();
+          } else {
+            return Text('Opps there was an error');
+          }
+        },
+      ),
+
       //weatherModel ==null ? const NoWeatherBody(): WeatherInfoBody(),// i use Ternary operator
     );
   }
