28a585609559709132c6fb6cedf36165503d198d
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-14 03:07:08 +0000

    provide cubit with tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/core/utils/go_router.dart';
+import 'package:weather_app/features/presentation/cubits/get_weather_cubit/get_weather_cubit.dart';
 
 
 void main() {
@@ -11,11 +13,14 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      //theme: ThemeData.light(),
-      debugShowCheckedModeBanner: false,
-      
+    return BlocProvider(
+      create: (context)=> GetWeatherCubit(),
+      child: MaterialApp.router(
+        routerConfig: router,
+        //theme: ThemeData.light(),
+        debugShowCheckedModeBanner: false,
+        
+      ),
     );
   }
 }
