7663167599c93104846a979cfcf51ecd7ba661b2
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-03-14 22:02:09 +0000

    add widgets  for forked future

---
+++ lib/features/widgets/custom_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/features/presentation/cubits/get_weather_cubit/get_weather_cubit.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   // BlocProvider.of<GetWeatherCubit>(context).weatherModel.cityName;
+    return Text(
+      BlocProvider.of<GetWeatherCubit>(context).weatherModel.cityName,
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+}
